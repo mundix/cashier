1ebd63dd1f8b6e3e3a56c4b2f42a09c496bf45fe
Edmundo Pichardo <ce.pichardo@gmail.com>
2020-10-15 07:40:22 -0400

    Refactored checkout page , now need js to complete the workflow

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -10,7 +10,12 @@ class CheckoutController extends Controller
     public function checkout($plan_id)
     {
         $plan = Plan::findOrFail($plan_id);
-        $intent = auth()->user()->createSetupIntent();
+        $intent = auth()->user()->createSetupIntent(); //this came from  User model billable
         return view('billing.checkout', compact('plan', 'intent'));
+    }
+
+    public function processCheckout()
+    {
+
     }
 }
--- resources/views/billing/checkout.blade.php
+++ resources/views/billing/checkout.blade.php
@@ -18,17 +18,52 @@
 
             <div class="w-full p-6">
                 <div class="flex mb-4 -mx-2">
-                    <form action="{{route('checkout.process')}}" method='POST'>
+                    <form action="{{route('checkout.process')}}" method='POST' class="w-full max-w-sm">
                         @csrf
-                        <input type="hidden" name='billing_plan_id' value="{{$plan->id}}">
+                        <div class="md:flex md:items-center mb-6">
+                            <div class="md:w-1/3">
+                              <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                                Billing Plan ID
+                              </label>
+                            </div>
+                            <div class="md:w-2/3">
+                              <input type="hidden" name='billing_plan_id' value="{{$plan->id}}"
+                              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                              id="inline-full-name"
+                              >
+                            </div>
+                        </div>
+
                         {{-- The stripe part copy and paste from the laravel stripe form part --}}
-                        <input id="card-holder-name" type="text">
+                        <div>
+                            <div class="md:flex md:items-center mb-6">
+                                <div class="md:w-1/3">
+                                  <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                                    Card Holder Name
+                                  </label>
+                                </div>
+                                <div class="md:w-2/3">
+                                  <input type="text"
+                                  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                  id="card-holder-name"
+                                  >
+                                </div>
+                            </div>
+
+                        </div>
                         <!-- Stripe Elements Placeholder -->
                         <div id="card-element"></div> <!-- this fill with dynamic js -->
 
-                        <button id="card-button" data-secret="{{ $intent->client_secret  }}">
-                            Pay ${{number_format($plan->price/100, 2)}}
-                        </button>
+                        <div class="md:flex md:items-center">
+                            <div class="md:w-1/3"></div>
+                            <div class="md:w-2/3">
+                              <button
+                              id="card-button" data-secret="{{ $intent->client_secret  }}"
+                              class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                              Pay ${{number_format($plan->price/100, 2)}}
+                              </button>
+                            </div>
+                          </div>
                     </form>
             </div>
             </div>
